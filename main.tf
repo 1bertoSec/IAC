f15a93819ebd045b1b3617071ec1bdee26ca43ee
Humberto <ecc_1berto@live.com>
2023-06-08 20:37:29 -0300

    teste

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0c65adc9a5c1b5d7c"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo "<h1>Feito com Terraform</h1>" > index.html
+                nohup busybox httpd -f -p 8080 &
+                 EOF
 
   tags = {
-    Name = "Minha Primeira Instancia Terraform"
+    Name = "Teste AWS"
   }
 }
